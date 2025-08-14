dd9f1fb6c985b8c55f4cc6246331b3bad3541aff
crossa-ayres <crossa@ayresassociates.com>
2025-08-14 11:40:18 -0600

    formatting

--- src/FlowRHAF_Home.py
+++ src/FlowRHAF_Home.py
@@ -17,9 +17,9 @@ st.markdown("- **Rate of Change Analysis**: Analyze the rate of change of flow d
 st.header("Getting Started")
 st.write("To get started, select one of the analysis options from the sidebar. Each analysis tool will guide you through the necessary inputs and provide visualizations and summaries of the results.")
 st.write("To test out the tool, the user can use the following USGS Station IDs:")
-st.markdown("- Peak Flow Analysis: 06721000 (South Platte River at Fort Lupton, CO.), period of record: 04/29/1929 - 08/12/2025")
-st.markdown("- Base Flow Analysis: 06752280 (Cache La Poudre River at Fort Collins, CO.), period of record: 10/01/1979 - 08/12/2025")
-st.markdown("- Rate of Change Analysis: 06752260 (Cache La Poudre River at Fort Collins, CO.) 04/08/1975 - 08/12/2025")
+st.markdown("- **Peak Flow Analysis**: 06721000 (South Platte River at Fort Lupton, CO.), period of record: 04/29/1929 - 08/12/2025")
+st.markdown("- **Base Flow Analysis**: 06752280 (Cache La Poudre River at Fort Collins, CO.), period of record: 10/01/1979 - 08/12/2025")
+st.markdown("- **Rate of Change Analysis**: 06752260 (Cache La Poudre River at Fort Collins, CO.) 04/08/1975 - 08/12/2025")
 st.markdown(f"#### For additional information on the RHAF, please visit the Coalition for the Poudre Watershed website at the following link:"\
                    " https://www.poudrewatershed.org/rhaf")
 
